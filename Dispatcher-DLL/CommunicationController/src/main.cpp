41b968a2a83a17091c4bec302a123b8ea50e84bf
Sebastian Straube <connec@sebastianstraube.org>
2013-02-12 02:12:30 +0100

    main test case

--- Dispatcher-DLL/CommunicationController/src/main.cpp
+++ Dispatcher-DLL/CommunicationController/src/main.cpp
@@ -6,36 +6,48 @@ using namespace desm;
 
 int _tmain(int argc, _TCHAR* argv[]) {
 	std::string data;
+
 	try {
 		CommunicationController *cc_server = new CommunicationController(CommunicationController::MODE_SERVER, "127.0.0.1", "", 27017);
 		CommunicationController *cc_client = new CommunicationController(CommunicationController::MODE_CLIENT, "127.0.0.1", "127.0.0.1", 27017);
 		
 		// send a string once - will be bounced back and forth in the loop
-		cc_client->send("muh");
+		cc_client->send("-CLIENT IS SENDING THIS DATA-");
 
+		/*
 		for(int i = 0; i < 5; ++i) {
 			// server send+receiver
 			while(cc_server->receive(data)) {
 				std::cout << "server got data: " << data << std::endl;
 				//cc_server->send(data + "[server]");
 			}
-			// client send+receiver
+			/* client send+receiver
 			while(cc_client->receive(data)) {
 				std::cout << "client got data: " << data << std::endl;
 				cc_client->send(data + "[client]");
 			}
 			Sleep(500);
 		}
-		std::cout << "shutting down client" << std::endl;
-		delete cc_client;
-		std::cout << "shutting down server" << std::endl;
-		delete cc_server;
+		*/
+
+		//Client and Server must delete himself,
+		//cause of this async t_main thread
+
+		//std::cout << "shutting down client" << std::endl;
+		//delete cc_client;
+		//std::cout << "shutting down server" << std::endl;
+		//delete cc_server;
 	} catch(const std::bad_alloc& e) {
 		std::cerr << "EXCEPTION: " << e.what() << std::endl;
 		return 1;
 	} catch(...) {
 		std::cerr << "UNKNOWN EXCEPTION!" << std::endl;
 		return 1;
+	}
+
+	while(TRUE){
+		Sleep(10000);
+		std::cout << ".";
 	}
 
 	system("pause");
