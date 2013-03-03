be21ec74d0abd738bff100ffe4ceed5eff85eca5
Maximilian Haupt <maximilian.haupt@native-instruments.de>
2013-03-03 22:29:25 +0100

    added separate projects for CC stuff

--- Dispatcher-DLL/src/client.cpp
+++ Dispatcher-DLL/src/client.cpp
@@ -9,18 +9,9 @@ using namespace desm;
 int _tmain(int argc, _TCHAR* argv[]) {
 	std::string data;
 	try {
-		CommunicationController *cc_server = new CommunicationController(CommunicationController::MODE_SERVER, "127.0.0.1", 27017);
-		Sleep(1000);
 		CommunicationController *cc_client = new CommunicationController(CommunicationController::MODE_CLIENT, "127.0.0.1", 27017);
-		// send a string once - will be bounced back and forth in the loop
 		cc_client->send("muh");
 		for(int i = 0; i < 5; ++i) {
-			// server send+receiver
-			while(cc_server->receive(data)) {
-				std::cout << "server got data: " << data << std::endl;
-				cc_server->send(data + "[server]");
-			}
-			// client send+receiver
 			while(cc_client->receive(data)) {
 				std::cout << "client got data: " << data << std::endl;
 				cc_client->send(data + "[client]");
@@ -29,8 +20,6 @@ int _tmain(int argc, _TCHAR* argv[]) {
 		}
 		std::cout << "shutting down client" << std::endl;
 		delete cc_client;
-		std::cout << "shutting down server" << std::endl;
-		delete cc_server;
 	} catch(const std::bad_alloc& e) {
 		std::cerr << "EXCEPTION: " << e.what() << std::endl;
 		return 1;
