e6c923f915fe13f4c0620a5eb814651a9aedf225
Maximilian Haupt <maximilian.haupt@native-instruments.de>
2013-07-31 15:11:27 +0200

    continued work on protocol in Middleware

--- Dispatcher-DLL/src/Middleware.cpp
+++ Dispatcher-DLL/src/Middleware.cpp
@@ -7,9 +7,11 @@
 #include <list>
 #include <map>
 #include <set>
+#include <vector>
 
 #include <json/json.h>
 
+#include "ErrorCodes.h"
 #include "Middleware.h"
 #include "Thread.h"
 
@@ -19,7 +21,39 @@ namespace desm {
 
 	enum MESSAGE_TYPE {
 		MESSAGE_TYPE_SET_KILOMETER_DIRECTION = 1,
+		MESSAGE_TYPE_SET_BALISE,
 		MESSAGE_TYPE_SET_ISOLIERSTOSS
+	};
+
+	namespace {
+
+		template<class T>
+		void deleteContainerEntries(T& begin, T& end) {
+			for(; begin != end; ++begin) {
+				delete *begin;
+			}
+		}
+		
+		struct Balise {
+			Balise(int _id, double _position, int _direction) : id(_id), position(_position), direction(_direction) {}
+			int id;
+			double position;
+			int direction;
+		};
+
+		struct Gleis {
+			Gleis(int _id = std::numeric_limits<int>::max()) : id(_id) {}
+			~Gleis() { deleteContainerEntries(balise.begin(), balise.end()); }
+			bool isValid() const { return this->id != std::numeric_limits<int>::max(); }
+			int id;
+			std::set<double> isolierstoesse;
+			std::vector<Balise*> balise;
+		};
+
+		struct tState {
+			int kilometerDirection;
+			std::map<int, Gleis> gleise;
+		};
 	};
 
 	struct Middleware::Impl {
@@ -28,18 +62,6 @@ namespace desm {
 		// types
 
 		typedef Thread<typename Middleware::Impl, void*> tFetchThread;
-
-		struct tGleis {
-			tGleis(int _id = std::numeric_limits<int>::max()) : id(_id) {}
-			bool isValid() const { return this->id != std::numeric_limits<int>::max(); }
-			int id;
-			std::set<double> isolierstoesse;
-		};
-
-		struct tState {
-			int kilometerDirection;
-			std::map<int, tGleis> gleise;
-		};
 
 		////////////////////////////////////////////////////////////////////////
 		// member
@@ -96,6 +118,8 @@ namespace desm {
 			m_state.kilometerDirection = 0;
 		}
 
+#pragma region begin message parsing
+
 		void sendMessage(int msgType, const Json::Value& v) {
 			Json::FastWriter writer;
 			Json::Value root;
@@ -126,31 +150,91 @@ namespace desm {
 					m_state.kilometerDirection = v.asInt();
 				}
 				break;
+			case MESSAGE_TYPE_SET_BALISE:
+				parseSetBalise(v);
+				break;
 			case MESSAGE_TYPE_SET_ISOLIERSTOSS:
-				if(v.isObject()) {
-					int gleisId = v.get("gleisId", Json::Value::maxInt).asInt();
-					double position = v.get("position", std::numeric_limits<double>::max()).asDouble();
-					if(gleisId != Json::Value::maxInt && position != std::numeric_limits<double>::max()) {
-						if(m_state.gleise.find(gleisId) != m_state.gleise.end()) {
-							m_state.gleise[gleisId].isolierstoesse.insert(position);
-						}
-					}
-				}
+				parseSetIsolierstoss(v);
 				break;
 			default:
 				std::cerr << "RECEIVED UNKNOWN MESSAGE " << msgType << std::endl;
 				break;
 			}
 		}
+		
+		void parseSetBalise(const Json::Value& v) {
+			if(!v.isObject()) {
+				return;
+			}
+			int gleisId = v.get("gleisId", Json::Value::maxInt).asInt();
+			int baliseId = v.get("baliseId", Json::Value::maxInt).asInt();
+			int direction = v.get("direction", Json::Value::maxInt).asInt();
+			double position = v.get("position", std::numeric_limits<double>::max()).asDouble();
+			if(gleisId == Json::Value::maxInt || 
+				baliseId == Json::Value::maxInt || 
+				direction == Json::Value::maxInt || 
+				position == std::numeric_limits<double>::max()) {
+				return;
+			}
+			if(setBaliseImpl(gleisId, baliseId, position, direction)) {
+				// TODO add balise event to event list for stw_getEvents();
+			}
+		}
+
+		void parseSetIsolierstoss(const Json::Value& v) {
+			if(!v.isObject()) {
+				return;
+			}
+			int gleisId = v.get("gleisId", Json::Value::maxInt).asInt();
+			double position = v.get("position", std::numeric_limits<double>::max()).asDouble();
+			if(gleisId == Json::Value::maxInt || position == std::numeric_limits<double>::max()) {
+				return;
+			}
+			if(setIsolierstossImpl(gleisId, position)) {
+				// TODO add isolierstoss event to event list for stw_getEvents();
+			}
+		}
+
+#pragma endregion
+
+#pragma region stats checks
+
+		bool isValidGleisId(int gleisId) const {
+			return m_state.gleise.find(gleisId) != m_state.gleise.end();
+		}
+
+#pragma endregion
+
+#pragma region state update
+
+		bool setIsolierstossImpl(int gleisId, double position) {
+			if(!isValidGleisId(gleisId)) {
+				return false;
+			}
+			if(m_state.gleise[gleisId].isolierstoesse.find(position) != m_state.gleise[gleisId].isolierstoesse.end()) {
+				return false;
+			}
+			m_state.gleise[gleisId].isolierstoesse.insert(position);
+			return true;
+		}
+
+		bool setBaliseImpl(int gleisId, double position, int baliseId, int direction) {
+			if(!isValidGleisId(gleisId)) {
+				return false;
+			}
+			// TODO check whether baliseId already exists. maybe switch from vector to map<id, Balise*> for that?
+			m_state.gleise[gleisId].balise.push_back(new Balise(baliseId, position, direction));
+			return true;
+		}
+
+#pragma endregion
 
 	};
 
 	////////////////////////////////////////////////////////////////////////////
 
 	Middleware::Middleware(const std::string& configPath)
-		: m_pImpl(new Impl(configPath))
-	{
-	}
+		: m_pImpl(new Impl(configPath)) {}
 
 	Middleware::~Middleware() {
 		delete m_pImpl;
@@ -162,38 +246,59 @@ namespace desm {
 	}
 
 	int Middleware::setTrack(int gleisId, double von, double bis, float abstand, const std::string& name) {
-		return 0;
+		if(!m_pImpl->isValidGleisId(gleisId)) {
+			return desm::ERROR_UNKNOWN_ID;
+		}
+		return desm::ERROR_OK;
 	}
 
 	int Middleware::setTrackConnection(int gleisId, int gleis1, int gleis2, double von, double bis, const std::string& name, int weiche1Id, int weiche2Id) {
+		if(!m_pImpl->isValidGleisId(gleisId)) {
+			return desm::ERROR_UNKNOWN_ID;
+		}
 		return 0;
 	}
 
 	int Middleware::setSignal (int signalId, int gleisId, double position, int typ, float hoehe, float distanz, const std::string& name, int direction) {
+		if(!m_pImpl->isValidGleisId(gleisId)) {
+			return desm::ERROR_UNKNOWN_ID;
+		}
 		return 0;
 	}
 
 	int Middleware::setBalise (int gleisId, double position, int baliseId, int direction) {
+		if(!m_pImpl->isValidGleisId(gleisId)) {
+			return desm::ERROR_UNKNOWN_ID;
+		}
+		if(m_pImpl->setBaliseImpl(gleisId, position, baliseId, direction)) {
+			Json::Value v(Json::objectValue);
+			v["gleisId"] = Json::Value(gleisId);
+			v["position"] = Json::Value(position);
+			v["baliseId"] = Json::Value(baliseId);
+			v["direction"] = Json::Value(direction);
+			m_pImpl->sendMessage(MESSAGE_TYPE_SET_BALISE, v);
+		}
 		return 0;
 	}
 
 	int Middleware::setLoop (int gleisId, double positionVon, double positionBis, int baliseId) {
+		if(!m_pImpl->isValidGleisId(gleisId)) {
+			return desm::ERROR_UNKNOWN_ID;
+		}
 		return 0;
 	}
 
 	int Middleware::setIsolierstoss (int gleisId, double position) {
-		if(m_pImpl->m_state.gleise.find(gleisId) == m_pImpl->m_state.gleise.end()) {
-			// TODO fehlercode für unbekanntes gleis
-			return EXIT_FAILURE;
+		if(!m_pImpl->isValidGleisId(gleisId)) {
+			return desm::ERROR_UNKNOWN_ID;
 		}
-		if(m_pImpl->m_state.gleise[gleisId].isolierstoesse.find(position) == m_pImpl->m_state.gleise[gleisId].isolierstoesse.end()) {
-			m_pImpl->m_state.gleise[gleisId].isolierstoesse.insert(position);
+		if(m_pImpl->setIsolierstossImpl(gleisId, position)) {
 			Json::Value v(Json::objectValue);
 			v["gleisId"] = Json::Value(gleisId);
 			v["position"] = Json::Value(position);
 			m_pImpl->sendMessage(MESSAGE_TYPE_SET_ISOLIERSTOSS, v);
 		}
-		return 0;
+		return desm::ERROR_OK;
 	}
 	
 	void Middleware::setKilometerDirection(int direction) {
